c0256bf2079ac6b317bcb9e702cdc599b9ce3ad8
katrina hockman and Phil Mass <katrina@>
2018-10-17 09:35:42 -0700

    add js to fill in mad lib.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    console.log("submitted");
+    var personInput = $("input#person").val();
+    var nounInput = $("input#noun").val();
+    var adjectiveInput = $("input#adjective").val();
+    var verbInput = $("input#verb").val();
+
+    $(".person").text(personInput);
+    $(".noun").text(nounInput);
+    $(".adjective").text(adjectiveInput);
+    $(".verb").text(verbInput);
+
+    $("#story").show();
+    console.log("submitted, " + personInput);
 
     event.preventDefault();
-  })
+  });
 });
