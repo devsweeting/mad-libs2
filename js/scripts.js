50262356d3fc37888c4501da0212e1c35393e42f
Devin Sweeting and Phil Mass <phil@>
2018-10-22 11:17:55 -0700

    update mad libs with loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var personInput = $("input#person").val();
-    var nounInput = $("input#noun").val();
-    var adjectiveInput = $("input#adjective").val();
-    var verbInput = $("input#verb").val();
 
-    $(".person").text(personInput);
-    $(".noun").text(nounInput);
-    $(".adjective").text(adjectiveInput);
-    $(".verb").text(verbInput);
+    var inputs = ["person", "noun", "adjective", "verb"]
+
+    inputs.forEach(function(input){
+      var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+      console.log("input=" + input + "  userInput=" + userInput);
+      debugger;
+    })
+
+    // var personInput = $("input#person").val();
+    // var nounInput = $("input#noun").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var verbInput = $("input#verb").val();
+    //
+    // $(".person").text(personInput);
+    // $(".noun").text(nounInput);
+    // $(".adjective").text(adjectiveInput);
+    // $(".verb").text(verbInput);
 
     $("#story").show();
-    console.log("submitted, " + personInput);
 
     event.preventDefault();
   });
